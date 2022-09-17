a1da56f42d3933a423e6226bd1fc64b2def28e39
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2022-09-17 23:44:26 +0900

    회원가입 기능 포스트맨으로 확인

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')  // 모듈 가져오기
 const app = express()             // express 앱 생성
 const port = 5500
-
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://msg:opCSUFdyT5kVIfXp@cluster0.w1uup9m.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...')).catch(err => console.log(err)) //mongoosedb
+const config = require('./config/key')
+// url 분석
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/',(req,res)=>{ res.send('Hello World!')}) // '/'디렉토리에 오면 Hello World 출력
+// Json 분석
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err)) //mongoosedb
+
+app.get('/',(req,res)=>{ res.send('Hello World! asdf')}) // '/'디렉토리에 오면 Hello World 출력
+
+app.post('/register',(req,res) => {  //회원가입 
+  const user = new User(req.body)
+  user.save((err,userInfo) => {
+    if(err) return res.json({succes:false,err})
+    return res.status(200).json({
+      succes: true
+    })
+  })
+})
 
 app.listen(port,() => {console.log(`Example app listening on port${port}`)})  // Port에서 실행
 
